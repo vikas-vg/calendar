e7d8fe1be6ed3fa71de61e5278f0454d380b35ec
Vikas Goyal <info.vikasgoyal@gmail.com>
2024-08-02 12:37:52 +0530

    remove warnining

--- calender_scheduler_view/models/ir_ui_view.py
+++ calender_scheduler_view/models/ir_ui_view.py
@@ -5,27 +5,18 @@ from odoo import fields, models
 
 TIMELINE_VIEW = ("scheduleview", "ScheduleCalender")
 
-VIEW_TYPES = [
-    ('tree', 'Tree'),
-    ('form', 'Form'),
-    ('graph', 'Graph'),
-    ('pivot', 'Pivot'),
-    ('calendar', 'Calendar'),
-    ('gantt', 'Gantt'),
-    ('kanban', 'Kanban'),
-    ("scheduleview", "ScheduleCalender")
-]
-
 
 class IrUIView(models.Model):
     _inherit = "ir.ui.view"
 
-    type = fields.Selection(selection_add=[TIMELINE_VIEW])
+    type = fields.Selection(selection_add=[TIMELINE_VIEW], ondelete={'scheduleview': 'cascade'})
+
 
 class ASctionView(models.Model):
     _inherit = "ir.actions.act_window.view"
 
-    view_mode = fields.Selection(VIEW_TYPES, string='View Type', required=True)
+    view_mode = fields.Selection(selection_add=[TIMELINE_VIEW], ondelete={'scheduleview': 'cascade'})
+
 
 class CalendarEvent(models.Model):
 
